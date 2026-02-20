eba5e56bcb036276f269930237c470d39d495ee2
haripriya-0712 <haripriyasudhakar712@gmail.com>
2026-02-20 09:51:17 +0530

    Display welcome messasge

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome checker App");
+        System.out.println("Version 1.0");
+        System.out.println("App initialized ");
+    }
+
+}
