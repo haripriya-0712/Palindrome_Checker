2c3d53e0330d85fa97ed4f562b967e4e8094dad6
haripriya-0712 <haripriyasudhakar712@gmail.com>
2026-02-27 12:01:16 +0530

    UC13 : Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,65 @@
-public class PalindromeCheckerApp {
+import java.util.*;
+
+public class PalindromeCheckerApp{
+
+    static boolean twoPointer(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    static boolean stackMethod(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray())
+            stack.push(c);
+
+        for (char c : str.toCharArray())
+            if (c != stack.pop())
+                return false;
+
+        return true;
+    }
+
+    static boolean dequeMethod(String str) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : str.toCharArray())
+            deque.addLast(c);
+
+        while (deque.size() > 1)
+            if (!deque.removeFirst().equals(deque.removeLast()))
+                return false;
+
+        return true;
+    }
 
     public static void main(String[] args) {
-        String word = "radar";
-        char[] ch = word.toCharArray();
-        int start = 0;
-        int end = ch.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (ch[start] != ch[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-        if (isPalindrome) {
-            System.out.println(word + " is Palindrome");
-        } else {
-            System.out.println(word + " is not a Palindrome");
-        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        long start, end;
+
+        start = System.nanoTime();
+        boolean r1 = twoPointer(input);
+        end = System.nanoTime();
+        System.out.println("Two Pointer Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean r2 = stackMethod(input);
+        end = System.nanoTime();
+        System.out.println("Stack Method Time: " + (end - start) + " ns");
+
+        start = System.nanoTime();
+        boolean r3 = dequeMethod(input);
+        end = System.nanoTime();
+        System.out.println("Deque Method Time: " + (end - start) + " ns");
+
+        sc.close();
     }
 }
